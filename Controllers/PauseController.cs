6ade8bcd91458f4a3577dbd03594fcf1b9da7cfc
yadirfd <formygithubproject@gmail.com>
2025-05-26 16:34:48 +0500

    Добавлена возможность поставить игру на паузу

---
+++ Controllers/PauseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Input;
+using lasthope.Views;
+using lasthope.Entities;
+
+namespace lasthope.Controllers
+{
+    public class PauseController
+    {
+        private readonly PauseView    _view;
+        private readonly InputHandler _input;
+
+        public PauseController(PauseView view, InputHandler input)
+        {
+            _view  = view;
+            _input = input;
+        }
+
+        // Пауза при нажатии на кнопку "Escape"
+        public bool Update()
+        {
+            return _input.IsNewKey(Keys.Escape);
+        }
+
+        public void Draw() => _view.Draw();
+    }
+}
