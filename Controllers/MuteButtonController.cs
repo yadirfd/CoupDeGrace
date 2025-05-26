c68bcaad99f9ffba496c02f8b09b2d975ff1dd9f
yadirfd <formygithubproject@gmail.com>
2025-05-26 16:34:01 +0500

    Добавлена возможность мута музыки на кнопку М

---
+++ Controllers/MuteButtonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Input;
+using lasthope.Entities;
+
+namespace lasthope.Controllers;
+
+public class MuteButtonController
+{
+    private readonly InputHandler  _input;
+    private readonly AudioManager  _audio;
+
+    public MuteButtonController(InputHandler input, AudioManager audio)
+    {
+        _input = input;
+        _audio = audio;
+    }
+
+    // Выключение музыки при нажатии на кнопку "М"
+    public bool Update()
+    {
+        if (_input.IsNewKey(Keys.M))
+        {
+            _audio.ToggleMute();
+            return true;
+        }
+        return false;
+    }
+}
