b7cdb5f73ce36b79cba0eae2a07dd49079fe9368
yadirfd <formygithubproject@gmail.com>
2025-05-26 16:46:49 +0500

    Добавлена иконка проигрыша/мута музыки на главном экране

---
+++ Views/MuteButtonView.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace lasthope.Views
+{
+    // Иконка мута
+    public class MuteButtonView
+    {
+        private readonly SpriteBatch _sb;
+        private readonly Texture2D _iconOn;
+        private readonly Texture2D _iconOff;
+        private readonly Vector2 _position;
+
+        public MuteButtonView(SpriteBatch sb, Texture2D iconOn, Texture2D iconOff, Vector2 position)
+        {
+            _sb = sb;
+            _iconOn = iconOn;
+            _iconOff = iconOff;
+            _position = position;
+        }
+
+        public void Draw(bool isMuted)
+        {
+            var icon = isMuted ? _iconOff : _iconOn;
+            _sb.Draw(icon, _position, Color.White);
+        }
+    }
+}
