60f318861bee70139544dd5b070fe12eac2037d1
yadirfd <formygithubproject@gmail.com>
2025-05-26 16:23:21 +0500

    Добавлен менеджер музыки

---
+++ Controllers/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Media;
+
+namespace lasthope.Controllers;
+
+public class AudioManager
+{
+    // Возможность отключить музыку
+    public bool IsMuted
+    {
+        get => MediaPlayer.IsMuted;
+        set => MediaPlayer.IsMuted = value;
+    }
+
+    public void ToggleMute()
+    {
+        IsMuted = !IsMuted;
+    }
+}
